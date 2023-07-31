c959e4a4cb965d9b924826d1b83f2a2bda6cac7a
Git88user <kollector789@gmail.com>
2023-08-01 00:54:22 +0300

    otvet_na_zadaniye-2.5

---
+++ zanyatie_2.5-script_dlya_vitrini.sql
@@ -0,0 +1,13 @@
+SELECT
+    ss.shop_name,
+    pr.product_name,
+    ss.sales_cnt AS sales_fact,
+    pl.plan_cnt AS sales_plan,
+    ss.sales_cnt / pl.plan_cnt AS sales_fact/sales_plan,
+    ss.sales_cnt * pr.price AS income_fact,
+    pl.plan_cnt * pr.price AS income_plan,
+    ss.sales_cnt * pr.price / (pl.plan_cnt * pr.price) AS income_fact/income_plan
+FROM
+    shop_sales AS ss 
+    JOIN plan AS pl ON ss.shop_id = pl.shop_id AND ss.product_id = pl.product_id
+    JOIN products AS pr ON ss.product_id = pl.product_id;
