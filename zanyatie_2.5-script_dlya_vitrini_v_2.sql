c1b288f2e9a99086ad87f98efb27e7a77590d576
Git88user <kollector789@gmail.com>
2023-08-01 20:07:00 +0300

    ispravlen_skript&shema

--- zanyatie_2.5-script_dlya_vitrini_v_2.sql
+++ zanyatie_2.5-script_dlya_vitrini_v_2.sql
@@ -1,5 +1,5 @@
 SELECT
-    ss.shop_name,
+    s.shop_name,
     pr.product_name,
     ss.sales_cnt AS sales_fact,
     pl.plan_cnt AS sales_plan,
@@ -9,5 +9,6 @@ SELECT
     ss.sales_cnt * pr.price / (pl.plan_cnt * pr.price) AS income_fact/income_plan
 FROM
     shop_sales AS ss 
+    JOIN shops AS s ON ss.shop_id = s.shop_id
     JOIN plan AS pl ON ss.shop_id = pl.shop_id AND ss.product_id = pl.product_id
-    JOIN products AS pr ON ss.product_id = pl.product_id;
+    JOIN products AS pr ON ss.product_id = pl.product_id
